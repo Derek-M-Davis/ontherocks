05bcb4bf6d342f1e3812ca2ac226f7c1281ab2eb
Derek <derekdavis408@gmail.com>
2019-11-21 15:42:29 -0800

    added bcrypt to create user password

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const bcrypt = require('bcrypt')
 const router = express.Router();
 const User = require('../models/users.js');
 
 // Create/Post Route
 router.post('/', (req, res) => {
+    req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
     User.create(req.body, (error, createdUser) => {
         res.json(createdUser)
     })
