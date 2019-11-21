33afb5445bfb75da4b8a6f9a553bec200098bcc0
Derek <derekdavis408@gmail.com>
2019-11-21 11:06:12 -0800

    added and checked angular connectviity

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express()
+const session = require('express-session')
 const db = mongoose.connection
 
 const PORT = process.env.PORT || 3000;
